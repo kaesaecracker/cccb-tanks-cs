fd3e195f6992bdb8a9ff370812d3994679d7c95a
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2024-04-14 18:50:20 +0200

    load maps from text files on server start

--- TanksServer/GameLogic/MapService.cs
+++ TanksServer/GameLogic/MapService.cs
@@ -1,3 +1,5 @@
+using System.IO;
+
 namespace TanksServer.GameLogic;
 
 internal sealed class MapService
@@ -7,31 +9,33 @@ internal sealed class MapService
     public const ushort TileSize = 8;
     public const ushort PixelsPerRow = TilesPerRow * TileSize;
     public const ushort PixelsPerColumn = TilesPerColumn * TileSize;
+    private readonly string _map;
+    private readonly ILogger<MapService> _logger;
 
-    private readonly string _map =
-        """
-            #######.##########################.#########
-            #...................##.....................#
-            #...................##.....................#
-            #.....####......................####.......#
-            #..........................................#
-            #............###...........###.............#
-            #............#...............#.............#
-            #...##.......#....#....#.....#......##.....#
-            #....#..............................#......#
-            .....#...#......................#...#.......
-            .....#...#......................#...#.......
-            #....#..............................#......#
-            #...##.......#....#....#.....#......##.....#
-            #............#...............#.............#
-            #............###...........###.............#
-            #..........................................#
-            #.....####......................####.......#
-            #...................##.....................#
-            #...................##.....................#
-            #######.##########################.#########
-            """
-            .ReplaceLineEndings(string.Empty);
+    private string[] LoadMaps() => Directory.EnumerateFiles("./assets/maps/", "*.txt")
+        .Select(LoadMap)
+        .Where(s => s != null)
+        .Select(s => s!)
+        .ToArray();
+
+    private string? LoadMap(string file)
+    {
+        var text = File.ReadAllText(file).ReplaceLineEndings(string.Empty).Trim();
+        if (text.Length != TilesPerColumn * TilesPerRow)
+        {
+            _logger.LogWarning("cannot load map {}: invalid length", file);
+            return null;
+        }
+
+        return text;
+    }
+
+    public MapService(ILogger<MapService> logger)
+    {
+        _logger = logger;
+        var maps = LoadMaps();
+        _map = maps[Random.Shared.Next(0, maps.Length)];
+    }
 
     private char this[int tileX, int tileY] => _map[tileX + tileY * TilesPerRow];
 
