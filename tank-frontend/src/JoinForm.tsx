a952d4227d966a21ed7f6ab109fbf554c8fce737
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2024-05-05 22:33:55 +0200

    extract dialog component

--- tank-frontend/src/JoinForm.tsx
+++ tank-frontend/src/JoinForm.tsx
@@ -1,10 +1,11 @@
 import {useState} from 'react';
-import './JoinForm.css';
+import {useMutation} from '@tanstack/react-query';
+
 import {makeApiUrl} from './serverCalls';
-import Column from './components/Column.tsx';
 import Button from './components/Button.tsx';
 import TextInput from './components/TextInput.tsx';
-import {useMutation} from '@tanstack/react-query';
+import Dialog from './components/Dialog.tsx';
+import './JoinForm.css';
 
 export default function JoinForm({onDone}: {
     onDone: (name: string) => void;
@@ -29,7 +30,7 @@ export default function JoinForm({onDone}: {
 
     const confirm = () => postPlayer.mutate({name});
 
-    return <Column className="JoinForm">
+    return <Dialog className="JoinForm">
         <h3> Enter your name to play </h3>
         <TextInput
             value={name}
@@ -42,5 +43,5 @@ export default function JoinForm({onDone}: {
             disabled={disableButtons}
             text="INSERT COIN"/>
         {postPlayer.isError && <p>{postPlayer.error.message}</p>}
-    </Column>;
+    </Dialog>;
 }
