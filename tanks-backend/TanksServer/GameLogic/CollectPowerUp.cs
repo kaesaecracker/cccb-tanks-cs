079b096c167680ef22187e896aaa794d6a2eff68
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2024-05-05 13:01:37 +0200

    allocate predicate once instead of per tick

--- tanks-backend/TanksServer/GameLogic/CollectPowerUp.cs
+++ tanks-backend/TanksServer/GameLogic/CollectPowerUp.cs
@@ -6,16 +6,18 @@ internal sealed class CollectPowerUp(
     MapEntityManager entityManager
 ) : ITickStep
 {
+    private readonly Predicate<PowerUp> _collectPredicate = b => TryCollect(b, entityManager.Tanks);
+
     public ValueTask TickAsync(TimeSpan delta)
     {
-        entityManager.RemoveWhere(TryCollect);
+        entityManager.RemoveWhere(_collectPredicate);
         return ValueTask.CompletedTask;
     }
 
-    private bool TryCollect(PowerUp obj)
+    private static bool TryCollect(PowerUp powerUp, IEnumerable<Tank> tanks)
     {
-        var position = obj.Position;
-        foreach (var tank in entityManager.Tanks)
+        var position = powerUp.Position;
+        foreach (var tank in tanks)
         {
             var (topLeft, bottomRight) = tank.Bounds;
             if (position.X < topLeft.X || position.X > bottomRight.X ||
@@ -24,36 +26,40 @@ internal sealed class CollectPowerUp(
 
             // now the tank overlaps the power up by at least 0.5 tiles
 
-            switch (obj.Type)
-            {
-                case PowerUpType.MagazineType:
-                    if (obj.MagazineType == null)
-                        throw new UnreachableException();
-
-                    tank.Magazine = tank.Magazine with
-                    {
-                        Type = tank.Magazine.Type | obj.MagazineType.Value,
-                        UsedBullets = 0
-                    };
-
-                    if (tank.ReloadingUntil >= DateTime.Now)
-                        tank.ReloadingUntil = DateTime.Now;
-
-                    break;
-                case PowerUpType.MagazineSize:
-                    tank.Magazine = tank.Magazine with
-                    {
-                        MaxBullets = (byte)int.Clamp(tank.Magazine.MaxBullets + 1, 1, 32)
-                    };
-                    break;
-                default:
-                    throw new UnreachableException();
-            }
-
+            ApplyPowerUpEffect(powerUp, tank);
             tank.Owner.Scores.PowerUpsCollected++;
             return true;
         }
 
         return false;
+    }
+
+    private static void ApplyPowerUpEffect(PowerUp powerUp, Tank tank)
+    {
+        switch (powerUp.Type)
+        {
+            case PowerUpType.MagazineType:
+                if (powerUp.MagazineType == null)
+                    throw new UnreachableException();
+
+                tank.Magazine = tank.Magazine with
+                {
+                    Type = tank.Magazine.Type | powerUp.MagazineType.Value,
+                    UsedBullets = 0
+                };
+
+                if (tank.ReloadingUntil >= DateTime.Now)
+                    tank.ReloadingUntil = DateTime.Now;
+
+                break;
+            case PowerUpType.MagazineSize:
+                tank.Magazine = tank.Magazine with
+                {
+                    MaxBullets = (byte)int.Clamp(tank.Magazine.MaxBullets + 1, 1, 32)
+                };
+                break;
+            default:
+                throw new UnreachableException();
+        }
     }
 }
