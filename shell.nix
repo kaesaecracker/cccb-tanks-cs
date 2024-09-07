6016d4e2d96e803f8b363d15e6471cd8f68add90
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2024-09-07 13:38:01 +0200

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,11 +1,23 @@
 {pkgs ? import <nixpkgs> {}}:
+let
+rust-toolchain = pkgs.symlinkJoin {
+    name = "rust-toolchain";
+    paths = with pkgs; [rustc cargo rustPlatform.rustcSrc rustfmt clippy];
+  };
+in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs.buildPackages; [
-    rustup
+    rust-toolchain
+
     pkg-config
     xe
     lzma
     cargo-tarpaulin
     gnumake
+    iconv
+
+    dotnet-sdk_8
   ];
+
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
